799971f2e1e97378d9da7e8de07656c56c94cd92
seven2202 <111497407+seven2202@users.noreply.github.com>
2025-05-21 21:22:54 +0800

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -141,7 +141,7 @@ ${DOMAIN1:+${DOMAIN1} {
         reverse_proxy localhost:9301 {
             header_up Host {host}
             header_up X-Real-IP {remote_host}
-            header_up X-Forwarded-For {http.request.remote.for}
+            header_up X-Forwarded-For {http.request.remote.host}
             header_up REMOTE-HOST {remote_host}
             }
         }
@@ -149,17 +149,17 @@ ${DOMAIN1:+${DOMAIN1} {
         reverse_proxy localhost:9301 {
             header_up Host {host}
             header_up X-Real-IP {remote_host}
-            header_up X-Forwarded-For {http.request.remote.for}
+            header_up X-Forwarded-For {http.request.remote.host}
             header_up REMOTE-HOST {remote_host}
             }
         }
    handle /app/* {
         reverse_proxy localhost:9301
         }
+   redir /expander /expander/
    handle /expander/* {
         reverse_proxy localhost:9301
         }
-  
    redir /list /list/
    handle_path /list/* {
        rewrite *  /app/index.html
